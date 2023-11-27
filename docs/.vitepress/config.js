661d59614e9bc42745152e8a8ff86ef70e1ad956
[huangrui] <983674684@qq.com>
2023-11-27 19:29:24 +0800

    静态

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,57 @@
+import {nestjsSidebar} from "./backend/nestjsSiderbar";
+import {colorfulSidebar} from "./colorfulLife/colorfulSiderbar";
+import { nginxSidebar } from "./backend/nginxSidebar";
+import {bigManSidebar} from "./bigMan/bigManSiderbar";
+import {bugSidebar} from "./bug/bugSiderbar";
+import {frontendNav} from "./frontend";
+import {backendNav} from "./backend";
+import {gitSidebar} from "./backend/gitSidebar";
+export default {
+	base: '/jinx/',
+	outDir: '../public',
+	title: '蒸汽机里の业余小码农',
+	description: 'Talk is cheap, show me your code.',
+	themeConfig: {
+		siteTitle: 'coder の life',
+		logo: '/img/logo.png',
+		nav: [
+			{
+				text: '首页',
+				link: '/index',
+			},
+			frontendNav,
+			backendNav,
+			{
+				text: '巨人的肩膀',
+				link: '/bigMan/',
+			},
+			{
+				text: 'bug集中营',
+				link: '/bug/',
+			},
+			{
+				text: '缤纷世界',
+				link: '/colorfulLife/',
+			},
+		],
+		sidebar: {
+			// 前端
+			// 后端
+			'/backend/nestjs/': nestjsSidebar,
+			'/backend/nginx/': nginxSidebar,
+			'/backend/gitLearn/': gitSidebar,
+			// 巨人的肩膀
+			'/bigMan/': bigManSidebar,
+			// bug集中营
+			'/bug/': bugSidebar,
+			// 缤纷世界
+			'/colorfulLife/': colorfulSidebar,
+		},
+
+		socialLinks: [
+			{icon: 'github', link: 'https://github.com/jinxmagnet/jinx'},
+		]
+
+	}
+
+}
---
+++ docs/index.md
@@ -0,0 +1,22 @@
+---
+layout: home
+
+hero:
+  name: 蒸汽机里の业余小码农
+  text: Just Coding... 
+  tagline: Talk is cheap, show me your code!
+  image:
+    src: /img/logo.png
+    alt: VitePress
+  actions:
+    - theme: brand
+      text:  Welcome!
+      link: /colorfulLife/
+    - theme: alt
+      text: GitHub
+      link: https://github.com/jinxmagnet/jinx
+features:
+  - icon: 🧑🏼‍💻‍
+    title: 这里还没想好写点什么
+    details: waiting...
+---
